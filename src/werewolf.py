be40f2a9be48aeee2f01f51d5d9bfbaf96d31c3c
Gustavo Marques <gustavomarques.gml@gmail.com>
2019-01-28 19:29:50 -0200

    Adition of join game

--- src/werewolf.py
+++ src/werewolf.py
@@ -9,22 +9,22 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
 driver = webdriver.Chrome('chromedriver', service_args=["--verbose", "--log-path=/tmp/CHROMIUM_LOG"], options=chrome_options)
 driver.get('https://web.whatsapp.com/')
-input('Insira o QR e aperte enter')
+find_user(driver, 'SLEEP')
 
 contacts = {}
 
-is_running = False
+is_game_running = False
 group_name = ''
 players = {}
 end_time = None
 
 while True:
-    if(is_running):
+    if(is_game_running):
         time_now = datetime.now()
         if(time_now > end_time):
             find_user(driver, group_name)
             send_text(driver, 'time_over')
-            is_running = False
+            is_game_running = False
             group_name = ''
             end_time = None
     try:
@@ -40,7 +40,10 @@ while True:
             print('Mensagens:', messages)
             # Comando para mensagens individuais
             if(chat.chat_type == 'normal'):
-                for message in messages[chat.name]:
+                if(not chat.name in messages):
+                    send_text(driver, 'bug_message')
+                    continue
+                for message in messages[chat.name][::-1]:
                     try:
                         run_command(driver, message)
                     except FileNotFoundError:
@@ -59,25 +62,39 @@ while True:
             # Comando para mensagens em grupo
             elif(chat.chat_type == 'group'):
                 for user in messages:
-                    for message in messages[user]:
+                    if(user == get_header(driver)):
+                        send_text(driver, 'bug_message')
+                        break
+                    for message in messages[user][::-1]:
+                        message = message.lower()
                         try:
                             run_command(driver, message)
                         except FileNotFoundError:
-                            if(message.lower() == 'start_game'):
-                                if(user in contacts):
-                                    time_now = datetime.now()
-                                    end_time = time_now + timedelta(minutes=1)
-                                    group_name = chat.name
-                                    is_running = True
-                                    send_text(driver, 'start_game', contacts[user])
+                            if(user in contacts):
+                                if(is_game_running):
+                                    if(message == 'join' and not user in players):
+                                        players[user]=None
+                                        send_text(driver, 'join', contacts[user])
+                                    elif(message == 'players'):
+                                        message = 'Jogadores:\n'
+                                        for number in list(players):
+                                            message += emoji.emojize(':bust_in_silhouette:'+ contacts[number]+'\n')
+                                        send_message(driver, message)
+                                    elif(message == 'force_start'):
+                                        pass
                                 else:
-                                    send_text(driver, 'unknown_user', user)
+                                    if(message == 'start_game'):
+                                        time_now = datetime.now()
+                                        end_time = time_now + timedelta(minutes=1)
+                                        group_name = chat.name
+                                        is_game_running = True
+                                        players[user]=None
+                                        send_text(driver, 'start_game', contacts[user])
                             else:
-                                pass
-            else:
-                pass
-        if('SLEEP' != get_header(driver)):    
-            find_user(driver, 'SLEEP')
-            time.sleep(3)
+                                send_text(driver, 'unknown_user', user)
+
+            if('SLEEP' != get_header(driver)):    
+                find_user(driver, 'SLEEP')
+                time.sleep(3)
     except NoSuchElementException:
         pass
